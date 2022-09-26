5fb4ffc5617982f3c97c5ca80aa7df06c7d6d15c
Chirinos <inversionesskysam@gmail.com>
2022-09-25 21:36:57 -0400

    V2.1.0 Actualizacion librerias/Ajuste en vista de Puntaje/Liberacion Play Store

--- app/src/main/java/com/skysam/hchirinos/mundialcatar/ui/points/PointsAdapter.kt
+++ app/src/main/java/com/skysam/hchirinos/mundialcatar/ui/points/PointsAdapter.kt
@@ -1,15 +1,20 @@
 package com.skysam.hchirinos.mundialcatar.ui.points
 
 import android.content.Context
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.color.MaterialColors
 import com.skysam.hchirinos.mundialcatar.R
 import com.skysam.hchirinos.mundialcatar.dataclass.User
+import com.skysam.hchirinos.mundialcatar.repositories.Auth
 
 /**
  * Created by Hector Chirinos on 11/05/2022.
@@ -39,6 +44,12 @@ class PointsAdapter(private val users: MutableList<User>): RecyclerView.Adapter<
             .circleCrop()
             .placeholder(R.drawable.ic_person_24)
             .into(holder.image)
+
+        if (item.id == Auth.getCurrenUser()!!.uid) {
+            holder.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.rosado_light))
+        } else {
+            holder.card.setCardBackgroundColor(getPrimaryColor())
+        }
     }
 
     override fun getItemCount(): Int = users.size
@@ -47,5 +58,12 @@ class PointsAdapter(private val users: MutableList<User>): RecyclerView.Adapter<
         val team: TextView = view.findViewById(R.id.tv_user)
         val image: ImageView = view.findViewById(R.id.iv_user)
         val points: TextView = view.findViewById(R.id.tv_points)
+        val card: MaterialCardView = view.findViewById(R.id.card)
+    }
+
+    private fun getPrimaryColor(): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(android.R.attr.colorBackground, typedValue, true)
+        return ContextCompat.getColor(context, typedValue.resourceId)
     }
 }
